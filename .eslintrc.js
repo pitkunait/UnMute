ca6d667cdf9fecb47e0d5ea974c72664c9f90326
Vassili <vassili@tftak.eu>
2020-12-07 18:48:08 +0200

    eslint ploho vse ravno nastroen

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    extends: [
+        "airbnb/hooks",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    plugins: ["react", "@typescript-eslint"],
+    env: {
+        browser: true,
+        es6: true,
+    },
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+    },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: "module",
+        project: "./tsconfig.json",
+    },
+    rules: {
+        "linebreak-style": "off",
+        indent: ["error", 4],
+    },
+};
