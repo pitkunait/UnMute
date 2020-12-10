9c907c1d5344271482a3c0a3d5f378fd4b7c54a2
Vassili <vassili@tftak.eu>
2020-12-10 17:17:19 +0200

    unmute

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,63 @@
 import React, { Component } from 'react';
-import { Route, Router, Switch } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-
-import NavBar from './components/NavBar/NavBar';
+import { Redirect, Route, Router, Switch } from 'react-router-dom';
 import Page0 from './components/Page0/Page0';
 import Page1 from './components/Page1/Page1';
-
-const history = createBrowserHistory();
+import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import { history } from './services/history';
 
 class App extends Component {
+    state = {
+        var3: 10,
+    };
+
+    incrementVar = () => {
+        this.setState({ var3: this.state.var3 + 1 });
+    };
+
     render() {
         return (
             <Router history={history}>
                 <h1 className="App">
-                    <NavBar />
+                    <Drawer variant="permanent">
+                        <List>
+                            <ListItem
+                                button
+                                style={{ width: 240 }}
+                                onClick={() => {
+                                    history.push('/page1');
+                                }}
+                            >
+                                <ListItemIcon>
+                                    <InboxIcon />
+                                </ListItemIcon>
+                                <ListItemText>Page1</ListItemText>
+                            </ListItem>
+
+                            <ListItem
+                                button
+                                style={{ width: 240 }}
+                                onClick={() => {
+                                    history.push('/page0');
+                                }}
+                            >
+                                <ListItemIcon>
+                                    <InboxIcon />
+                                </ListItemIcon>
+
+                                <ListItemText primary={'Page2'} />
+                            </ListItem>
+                        </List>
+                    </Drawer>
+
                     <Switch>
                         <Route path={'/page0'} component={Page0} />
-                        <Route path={'/page1'} component={Page1} />
+
+                        <Route path={'/page1'}>
+                            <Page1 var3={this.state.var3} incrementVar={this.incrementVar} />
+                        </Route>
+
+                        <Redirect to={'/page1'} />
                     </Switch>
                 </h1>
             </Router>
--- src/components/Page1/Page1.tsx
+++ src/components/Page1/Page1.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { history } from '../../services/history';
 
-const Page1 = () => {
-    return <div>this is page 1</div>;
+interface Page1Props {
+    var3: number;
+    incrementVar: any;
+}
+
+const Page1 = (props: Page1Props) => {
+    let variable = 1;
+
+    const [var2, setVar2] = useState(5);
+
+    const onClickHandler = () => {
+        console.log('suka nazhala !');
+        variable++;
+        setVar2(var2 + 1);
+        console.log(variable);
+        console.log(var2);
+        props.incrementVar();
+    };
+
+    const goNahuj = () => {
+        console.log('123');
+        history.push('/page0');
+    };
+
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <div>This is shit: {variable}</div>
+            <div>This is from useState hook: {var2}</div>
+            <div>This is from prop: {props.var3}</div>
+
+            <button onClick={onClickHandler}>CLICK ME SUKA</button>
+
+            <button onClick={goNahuj}>Go to page</button>
+        </div>
+    );
 };
 
 export default Page1;
