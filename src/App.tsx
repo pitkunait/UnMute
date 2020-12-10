68d56e86f7150b2e238766da63a96502f3f9990d
rnrn <nikita@tftak.eu>
2020-12-10 18:29:40 +0200

    added a shart

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Redirect, Route, Router, Switch } from 'react-router-dom';
-import Page0 from './components/Page0/Page0';
-import Page1 from './components/Page1/Page1';
 import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import { history } from './services/history';
+
+import Page0 from './components/Page0/Page0';
+import Page1 from './components/Page1/Page1';
+import Chart from './components/Chart/Chart';
 
 class App extends Component {
     state = {
@@ -38,23 +40,25 @@ class App extends Component {
                                 button
                                 style={{ width: 240 }}
                                 onClick={() => {
-                                    history.push('/page0');
+                                    history.push('/chart');
                                 }}
                             >
                                 <ListItemIcon>
                                     <InboxIcon />
                                 </ListItemIcon>
-
-                                <ListItemText primary={'Page2'} />
+                                <ListItemText>ClickPie</ListItemText>
                             </ListItem>
                         </List>
                     </Drawer>
 
+                    {/* here comes the switch */}
                     <Switch>
-                        <Route path={'/page0'} component={Page0} />
-
                         <Route path={'/page1'}>
                             <Page1 var3={this.state.var3} incrementVar={this.incrementVar} />
+                        </Route>
+
+                        <Route path={'/chart'}>
+                            <Chart />
                         </Route>
 
                         <Redirect to={'/page1'} />
